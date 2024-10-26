18cf47419c0acefda30a10fa28fa183fa5522b2f
Cleber Gomes <fclebinho@gmail.com>
2024-10-26 12:31:56 -0300

    fix: layout

--- src/components/client-msg.tsx
+++ src/components/client-msg.tsx
@@ -3,7 +3,14 @@
 import React from "react";
 
 const ClientMessage: React.FC = () => {
-  return <p>ClientMessage: {process.env.NEXT_PUBLIC_MSG ?? "No message"}</p>;
+  return (
+    <span>
+      ClientMessage:{" "}
+      <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+        {process.env.NEXT_PUBLIC_MSG ?? "No message"}
+      </code>
+    </span>
+  );
 };
 
 export default ClientMessage;
--- src/components/server-msg.tsx
+++ src/components/server-msg.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ServerMessage: React.FC = () => {
-  return <p>ServerMessage: {process.env.NEXT_PUBLIC_MSG ?? "No message"}</p>;
+  return (
+    <span>
+      ServerMessage:{" "}
+      <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+        {process.env.NEXT_PUBLIC_MSG ?? "No message"}
+      </code>
+    </span>
+  );
 };
 
 export default ServerMessage;
